030eaf768410564023c571d9cd01007b91ef1ec5
bitjungle <devel@bitjungle.com>
2020-09-07 08:19:13 +0200

    utforsk oddetall

---
+++ utforsk/1.5-oddetall.py
@@ -0,0 +1,7 @@
+oddesum = 0
+for n in range(10):
+    oddetall = 2*n + 1
+    oddesum = oddesum + oddetall
+    print(oddetall, " - sum:", oddesum)
+
+# Sn = (2n - 1) + (2(n-1) + 1) +  (2(n-2) + 1) + ... + 1
